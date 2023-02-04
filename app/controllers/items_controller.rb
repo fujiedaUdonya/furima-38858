bbe21de8186ebd88968de8ce6d0e048c534b8b57
fujieUdonya <suizouharetsu1110@hotmail.co.jp>
2023-02-04 11:20:49 +0900

    set_user記述修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -53,9 +53,9 @@ class ItemsController < ApplicationController
   end
 
   def set_user
-    if @items.user_id !== current_user.id 
+    if @items.user_id != current_user.id 
        redirect_to root_path
     end
   end
-
+  
 end
