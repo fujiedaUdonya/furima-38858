b1fd3ce1bab56f019d20c06a8ba4c0f084878fce
fujieUdonya <suizouharetsu1110@hotmail.co.jp>
2023-02-11 19:42:14 +0900

    修正3:商品購入機能

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_04_092032) do
+ActiveRecord::Schema.define(version: 2023_02_11_193700) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2023_02_04_092032) do
     t.integer "prefecture_id", null: false
     t.string "city", null: false
     t.string "house_number", null: false
-    t.string "other", null: false
+    t.string "other"
     t.string "phone_number", null: false
     t.bigint "purchase_id", null: false
     t.datetime "created_at", precision: 6, null: false
