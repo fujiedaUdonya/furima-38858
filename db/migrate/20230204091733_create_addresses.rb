d78fee922979c1c67592f4e5234633236db76153
fujieUdonya <suizouharetsu1110@hotmail.co.jp>
2023-02-11 17:59:47 +0900

    修正2:商品購入機能

--- db/migrate/20230204091733_create_addresses.rb
+++ db/migrate/20230204091733_create_addresses.rb
@@ -6,7 +6,7 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
       t.integer    :prefecture_id,  null: false
       t.string     :city,           null: false
       t.string     :house_number,   null: false
-      t.string     :other,          null: false
+      t.string     :other
       t.string     :phone_number,   null: false
       t.references :purchase,       null: false, foreign_key: true
       
